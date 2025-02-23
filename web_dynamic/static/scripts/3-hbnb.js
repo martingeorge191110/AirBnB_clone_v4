ebd4bbeb3b54a11f8bd4b463c195c1178fc7805f
Martin George <martingeorge191110@gmail.com>
2025-02-23 18:07:18 +0200

    Task 3, Chmod of file task 2

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,56 @@
+$(document).ready(() => {
+    const amenities = {};
+
+    $('input[type="checkbox"]').change(function () {
+      if ($(this).is(":checked")) {
+        amenities[$(this).attr("data-id")] = $(this).attr("data-name");
+      } else {
+        delete amenities[$(this).attr("data-id")];
+      }
+
+      $(".amenities h4").text(Object.values(amenities).join(", "));
+    });
+
+    const apiVar = $("#api_status");
+    fetch('http://0.0.0.0:5001/api/v1/status/')
+      .then(response => response.json())
+      .then(data => {
+          if (data.status === 'OK') {
+              apiVar.addClass("available");
+          } else {
+              apiVar.removeClass("available");
+          }
+
+        })
+      .catch(error => console.error("API Fetch Error:", error));
+
+
+    fetch('http://127.0.0.1:5001/api/v1/places_search/', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({})
+    })
+    .then(response => response.json())
+    .then(data => {
+        for (const place of data) {
+            const html = `<article>
+                            <div class="title_box">
+                                <h2>${place.name}</h2>
+                                <div class="price_by_night">$${place.price_by_night}</div>
+                            </div>
+                            <div class="information">
+                                <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
+                                <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
+                                <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+                            </div>
+                            <div class="description">
+                                ${place.description}
+                            </div>
+                        </article>`;
+            $('section.places').append(html);
+        }
+    })
+    .catch(error => console.error("Error fetching places:", error));
+});
